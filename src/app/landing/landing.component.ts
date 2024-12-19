d3c37d7bc77d3c0caccb5fc61d64739f9fd61468
edgardoni <edgardoni@gmail.com>
2024-12-18 22:35:51 -0300

    site updated

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  imports: [],
+  templateUrl: './landing.component.html',
+  styleUrls: [
+    '../../assets/css/bootstrap.min.css',
+    '../../assets/css/owl.carousel.min.css',
+    '../../assets/css/magnific-popup.css',
+    '../../assets/css/font-awesome.min.css',
+    '../../assets/css/themify-icons.css',
+    '../../assets/css/nice-select.css',
+    '../../assets/css/gijgo.css',
+    '../../assets/css/flaticon.css',
+    '../../assets/css/slicknav.css',
+    '../../assets/css/style.css',
+    './landing.component.css',
+  ]
+})
+export class LandingComponent {
+
+}
