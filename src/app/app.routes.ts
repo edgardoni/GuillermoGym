22490cc31182c22913c7dfc7eb66891d53543750
edgardoni <edgardoni@gmail.com>
2024-12-18 22:49:26 -0300

    static site

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
-
-export const routes: Routes = [
-    { path: 'landing', component: LandingComponent },
-    { path: '', redirectTo: '/landing', pathMatch: 'full' }
-  ];
